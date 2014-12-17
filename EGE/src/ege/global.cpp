35cf6ff35d82857f80b8b7f1c5b9dcd9f9deeeb9
FrankHB <frankhb1989@gmail.com>
2014-12-18 01:41:53 +0800

    EGE/src/ege/: 提取反初始化实现。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- EGE/src/ege/global.cpp
+++ EGE/src/ege/global.cpp
@@ -178,7 +178,6 @@ _graph_setting::_graph_setting(int gdriver_n, int* gmode)
 _graph_setting::~_graph_setting()
 {
 	yassume(!ys_thrd.joinable());
-
 	ys_pnl.reset();
 }
 
@@ -774,6 +773,17 @@ _graph_setting::_update()
 			SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOOWNERZORDER | SWP_NOZORDER);
 	}
 	return grOk;
+}
+
+void
+_graph_setting::_uninit()
+{
+	::ShowWindow(_get_hwnd(), SW_HIDE);
+	YSLib::PostQuitMessage(0);
+	if(ui_thread.joinable())
+		ui_thread.join();
+	if(ys_thrd.joinable())
+		ys_thrd.join();
 }
 
 void
